b7c9e68c3a2b75e5c264010a56d5894591d4bf98
Vandana <vandana182607@gmail.com>
2025-07-16 12:20:27 +0530

    Remove myenv from repo and add to .gitignore

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+
+name='mlproject',
+version='0.0.1',
+author='Vandana',
+author_email='vandana182607@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+
+)
